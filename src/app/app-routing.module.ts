b6235daab8170997bcf248d87779bd38dc7da197
maryfranklinnn <mary@fuse.sg>
2020-10-23 16:44:09 +0700

    MultiState Animation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { FadeInComponent } from './fade-in/fade-in.component';
+import { ImgAnimationComponent } from './img-animation/img-animation.component';
 
 const routes: Routes = [
   { path: 'fade', component: FadeInComponent },
+  { path: 'animation1', component: ImgAnimationComponent },
   { path: '', redirectTo:'/fade', pathMatch:'full' },
 ];
 
--- src/app/img-animation/img-animation.component.ts
+++ src/app/img-animation/img-animation.component.ts
@@ -1,15 +1,36 @@
+import { animate, state, style, transition, trigger } from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-img-animation',
   templateUrl: './img-animation.component.html',
-  styleUrls: ['./img-animation.component.css']
+  styleUrls: ['./img-animation.component.css'],
+  animations: [
+    trigger('photoState',[
+      state('move',style({
+        transform : 'translateX(25%) translateY(50px)',
+      })),
+      state('spin',style({
+        transform : 'rotateY(45deg) rotateZ(45deg)',
+      })),
+      state('enlarge',style({
+        transform : 'scale(1.5)',
+      })),
+      transition('spin => move', animate('200ms ease-out')),
+      transition('* => *', animate('500ms ease-out')),
+    ])
+  ]
 })
 export class ImgAnimationComponent implements OnInit {
 
   constructor() { }
 
+  position: string;
+  photoUrl = 'https://pbs.twimg.com/media/D0XyRv6WkAEgVFN?format=jpg&name=large';
+
   ngOnInit(): void {
   }
-
+  changePosition(newPosition : string){
+    this.position = newPosition;
+  }
 }
