f3ba824512765a326a0dff6b07ba6b4de0b50739
maryfranklinnn <mary@fuse.sg>
2020-10-26 12:03:35 +0700

    Stack Animation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { FadeInComponent } from './fade-in/fade-in.component';
 import { ImgAnimationComponent } from './img-animation/img-animation.component';
 import { ImgAnimation2Component } from './img-animation2/img-animation2.component';
+import { StackedImgComponent } from './stacked-img/stacked-img.component';
 
 const routes: Routes = [
   { path: 'fade', component: FadeInComponent },
   { path: 'animation1', component: ImgAnimationComponent },
   { path: 'animation2', component: ImgAnimation2Component },
+  { path: 'stack', component: StackedImgComponent },
   { path: '', redirectTo:'/fade', pathMatch:'full' },
 ];
 
--- src/app/stacked-img/stacked-img.component.ts
+++ src/app/stacked-img/stacked-img.component.ts
@@ -1,12 +1,25 @@
+import { animate, query, stagger, style, transition, trigger } from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-stacked-img',
   templateUrl: './stacked-img.component.html',
-  styleUrls: ['./stacked-img.component.css']
+  styleUrls: ['./stacked-img.component.css'],
+  animations: [
+    trigger('stackAnimation',[
+      transition('* => *', [
+        query('img', style({transform: 'translateX(-100%'})),
+        query('img', stagger('600ms', [
+          animate('900ms', style({transform: 'translateX(0'}))
+        ]))
+      ])
+    ])
+  ]
 })
 export class StackedImgComponent implements OnInit {
-
+  photos = ['https://kpopping.com/uploads/documents/54/4/20180506010144_liv_9076-copy.jpeg',
+  'https://imgnews.pstatic.net/image/origin/433/2020/06/01/69047.jpg',
+  'https://pbs.twimg.com/media/EW5Yu53U0AAya3I?format=jpg&name=large']
   constructor() { }
 
   ngOnInit(): void {
